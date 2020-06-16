8cfdd1836908d1c6c9af775ec6148ff171294d68
anonanonymous <23041331+anonanonymous@users.noreply.github.com>
2020-06-15 20:12:25 -0700

    shellnet wallet

---
+++ services/wallet/config.go
@@ -0,0 +1,20 @@
+package main
+
+const (
+	// Forking config.
+	divisor        = 100 // This is 100 for TRTL
+	transactionFee = 10  // This is 10 for TRTL
+	// walllet api connection
+	walletAPIKey  = ""
+	walletAPIPort = "8070"
+	walletAPIHost = "127.0.0.1"
+	// database credentials
+	dbUser = ""
+	dbPwd  = ""
+)
+
+var (
+	// network configuration
+	hostURI  = "http://localhost"
+	hostPort = ":8082"
+)
---
+++ services/wallet/go.mod
@@ -0,0 +1,9 @@
+module github.com/anonanonymous/shellnet/wallet
+
+go 1.12
+
+require (
+	github.com/anonanonymous/wallet-api-go v0.0.0-20190524001819-95e2b0752394
+	github.com/julienschmidt/httprouter v1.2.0
+	github.com/lib/pq v1.1.1
+)
